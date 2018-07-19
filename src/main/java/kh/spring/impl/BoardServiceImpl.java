0ba7f64cae7ef0ce550299ed57931af7b8d79719
jjh <jang6-1@hanmail.net>
2018-07-19 17:48:45 +0900

    에러 수정 - 재호
    bean 에러

--- src/main/java/kh/spring/impl/BoardServiceImpl.java
+++ src/main/java/kh/spring/impl/BoardServiceImpl.java
@@ -1,5 +1,17 @@
 package kh.spring.impl;
 
-public class BoardServiceImpl {
+import org.springframework.stereotype.Service;
+
+import kh.spring.dto.BoardDTO;
+import kh.spring.interfaces.BoardService;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Override
+	public int writing(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
