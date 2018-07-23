8252ae4fadb3e1de0f7506a56b4da23072357129
y2kim <kchy0806@naver.com>
2018-07-23 11:44:14 +0900

    before around  2개 만들었으니  둘중 하나 선택  하시면 될듯- 찬연

--- src/main/java/kh/spring/aspect/PeaceAspect.java
+++ src/main/java/kh/spring/aspect/PeaceAspect.java
@@ -1,10 +1,15 @@
 package kh.spring.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import kh.security.util.EncrpyUtils;
+import kh.spring.dto.MemberDTO;
 
 @Aspect
 @Component
@@ -15,7 +20,22 @@ public class PeaceAspect {
 	
 	@Before("secretPassword()")
 	public void setSecretpw(JoinPoint jp) {
-		
+		MemberDTO dto = (MemberDTO) jp.getArgs()[0];
+		dto.setPw(EncrpyUtils.getSha256(dto.getPw()));
+	}
+	
+	@Around("secretPassword()")
+	public int setSecretpw(ProceedingJoinPoint pjp) {
+		MemberDTO dto = (MemberDTO) pjp.getArgs()[0];
+		dto.setPw(EncrpyUtils.getSha256(dto.getPw()));
+		int result = 0 ;
+		try {
+			result = (Integer) pjp.proceed(new Object[] {dto});
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 }
